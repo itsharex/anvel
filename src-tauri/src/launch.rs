8249b0d8bf351d534e23b645dec1c4c23cddf04b
imrany <imranmat254@gmail.com>
2024-03-24 09:08:39 +0300

    update

--- src-tauri/src/launch.rs
+++
@@ -1,5 +0,0 @@
-use open::that;
-
-pub async fn launch_browser(url:&String)->Result<(),std::io::Error>{
-   that(&url)
-}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,36 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use clap::{
-    Parser,
-    Subcommand,
-};
 use dirs;
-use rand::{thread_rng, Rng};
 use actix_web::{
     HttpServer,
     http,
     App,
-    dev::{
-        ServiceRequest, 
-        ServiceResponse, 
-        fn_service
-    },
     web,
-};
-use actix_files::{
-    Files,
-    NamedFile
 };
 use actix_cors::Cors;
 use std::{
-    env::current_exe,
     net::Ipv4Addr,
-    path::{Path,PathBuf}
+    path::PathBuf,
 };
-use local_ip_address::local_ip;
-mod launch;
-use launch::launch_browser;
 
 #[path="./routes.rs"]
 mod routes;
@@ -44,25 +26,6 @@ use routes::{
     download,
     // get_shared_folder_contents,
 };
-
-#[derive(Parser)]
-#[command(author="Imrany <imranmat254@gmail.com>", version, about="A networking program for file sharing.", long_about = None)]
-struct Args {
-    /// Path to the folder you want to serve
-    #[arg(short, long, value_name= "PATH")]
-    root: Option<String>,
-
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Serves a specific folder.
-    Serve {
-        path: Option<String>,
-    },
-}
 
 fn get_root_directory() -> Option<PathBuf> {
     // On Unix-like systems (Linux, macOS), the root directory is "/"
@@ -83,29 +46,6 @@ fn get_root_directory() -> Option<PathBuf> {
     #[cfg(not(any(unix, windows)))]
     {
         None
-    }
-}
-
-#[actix_web::main]
-async fn main(){
-    let args = Args::parse();
-    if let Some(path) = args.root.as_deref() {
-        serve_me(path.to_string()).await;
-    }
-    
-    match &args.command {
-        Some(Commands::Serve { path }) => {
-            if let Some(path) = path.as_deref() {
-                serve_me(path.to_string()).await;
-            }else {
-                println!("  ERROR Specify a path to serve.");
-                println!(" {}",format!(" HINT: To serve the current folder - 'anvel serve ./'."));
-            }
-
-        }
-        None => {
-            start_anvel();
-        }
     }
 }
 
@@ -155,53 +95,13 @@ async fn serve_anvel(){
     .unwrap();
 }
 
-async fn serve_me(path: String) {
-    let port:u16=thread_rng().gen_range(3000..=8080);
-    let ipv4: (Ipv4Addr, u16)=("0.0.0.0".parse().unwrap(),port);
-    let server=HttpServer::new(move ||
-        App::new()
-            .service(Files::new("/", path.clone()).show_files_listing().index_file("index.html")
-                .default_handler(fn_service(|req: ServiceRequest| async {
-                    let (req, _) = req.into_parts();
-                    let current_exe_path=PathBuf::from(current_exe().unwrap());
-                    let file = NamedFile::open_async(Path::new(current_exe_path.parent().unwrap()).join("static_files/404.html")).await?;
-                    let res = file.into_response(&req);
-                    Ok(ServiceResponse::new(req, res))
-                }))
-            )
-    )
-    .bind(ipv4);
-    match server {
-        Ok(serve) => {
-            let url=format!("http://localhost:{port}/");
-            if let Err(e) = launch_browser(&url).await {
-                println!(" ERROR An error occurred when opening {url} {e}");
-            }else{
-                let my_local_ip = local_ip();
-                println!(" Local: {url}");
-                
-                match my_local_ip {
-                    Ok(ip) => {
-                        println!(" Network: {}",format!("http://{ip}:{port}/"));
-                    },
-                    Err(e) => {
-                        println!(" {} {}.", format!(" WARNING "),e );
-                    }
-                }
-            };
-            serve.run().await.unwrap_or_else(|err| println!(" ERROR {} ",err));
-        },
-        Err(e) =>  println!(" {} ",e)
-    }
-}
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn start_anvel() {
+fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet,serve_anvel])
         .run(tauri::generate_context!())
